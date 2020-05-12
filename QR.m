6a258f452c31490ce258d7847801916e51c182cf
rhartiv <rhartiv@users.noreply.github.com>
2020-05-12 01:34:44 -0400

    Update QR.m

--- QR.m
+++ QR.m
@@ -1,4 +1,25 @@
 %QR Method
-[Q,R]=qr(A)
+function [Vals,Vecs,k]=QR(A)
 
-% :)
+kmax=250
+copyA=A
+[Q,R]=qr(copyA)
+k=1
+while k<kmax
+    if istriu(round(Q'*copyA*Q*10000))==1
+        valmatrix=Q'*copyA*Q
+        for i=1:1:length(valmatrix)
+            Vals(i)=valmatrix(i,i)
+        end
+        break
+    elseif k==kmax-1
+        valmatrix=Q'*copyA*Q
+        for i=1:1:length(valmatrix)
+            Vals(i)=valmatrix(i,i)
+        end
+        break
+    end
+    copyA=Q'*copyA*Q
+    [Q,R]=qr(copyA)
+    k=k+1
+end
